d3eeb40667e46f860afee572dd3599438ed75d78
Glen Starchman <glen@starchman.com>
2016-07-01 06:28:08 -0700

    cleanup

--- src/main/scala/maiden/http/Render.scala
+++ src/main/scala/maiden/http/Render.scala
@@ -9,26 +9,17 @@ import maiden.types.Exceptions._
   */
 object Render {
 
-  def render2[E <: MaidenException, T](body:  => Either[E, T]) = {
-    body match {
-      case Right(b) => Ok(b)
-      //
-      case Left(e) => InternalServerError(new Exception(e.message))
-    }
-    //} handle {
-    //  case e: Exception => { log.errorST("Render Exception", e); BadRequest(e) }
-  }
-
   def render[T](body: => T) = {
     val result: Either[MaidenException, T] = Try (body) match {
       case Success(x) => Right(x)
       //TODO:  handle speciality cases here
-      case Failure(e) => { println(e); Left(MaidenRenderException) }
+      case Failure(e) => Left(MaidenRenderException)
     }
     result match {
       case Right(b) => Ok(b)
       //TODO: pattern match on exception so we return the right status code
-      case Left(e) =>  InternalServerError(new Exception(e.message))
+      case Left(e) => InternalServerError(new Exception(e.message))
     }
   }
+
 }
