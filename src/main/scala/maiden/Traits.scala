76054a4c4ba6bde3a512ded094452a287ed1cdae
Glen Starchman <glen@starchman.com>
2016-07-01 06:40:28 -0700

    cleanup

--- src/main/scala/maiden/Traits.scala
+++ src/main/scala/maiden/Traits.scala
@@ -9,7 +9,7 @@ trait WithAuthorization
 trait MaidenFullResponse extends MaidenEncoder
 
 //some helper traits
-
+//these are not actually used right now
 trait ApiModel extends MaidenModel with WithApi
 trait AdminableModel extends MaidenModel with WithApi with WithAdmin
 trait AuthorizedModelApi extends MaidenModel with WithApi with WithAuthorization
