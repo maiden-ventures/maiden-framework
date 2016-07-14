87254a0ee691b86f6b91996c838c2963635de020
Glen Starchman <glen@starchman.com>
2016-07-14 07:51:43 -0700

    better validations and formatters

--- src/main/scala/maiden/processing/Formatters.scala
+++ src/main/scala/maiden/processing/Formatters.scala
@@ -8,15 +8,11 @@ import io.circe.parser._
 //various formatters for various types
 object Formatters {
 
-  implicit def optionstring2string(s: Option[String]): String = s match {
-    case Some(x) => x
-    case _ => ""
-  }
-
   val phoneUtil = PhoneNumberUtil.getInstance
 
   abstract class StringFormatterOps(value: String) {
-    def phone(country: String = "US") = {
+
+    def phone(country: String = "US"): String = {
       val parsed = phoneUtil.parse(value, country)
       phoneUtil.formatInOriginalFormat(parsed, country)
     }
@@ -28,6 +24,16 @@ object Formatters {
     }
 
     def postal_code: String = postal_code("US")
+
+    //TODO: error checking and convert to regex
+    def social_security = {
+      val s = value.replace("-", "")
+      "%s-%s-%s".format(
+        s.substring(0,3),
+        s.substring(3,5),
+        s.substring(5,9)
+      )
+    }
 
     def list(sep: String) = value.split(sep).map(_.trim).mkString(",")
 
@@ -42,7 +48,29 @@ object Formatters {
   implicit class ImplicitStringFormatterOps(s: String) extends StringFormatterOps(s)
 
   //TODO: FIX THIS... it more than likely does not do what we want
-  implicit class ImplicitOptionStringFormatterOps(s: Option[String])
-      extends StringFormatterOps(s.getOrElse(""))
+  implicit class ImplicitOptionStringFormatterOps(s: Option[String]) {
+    val v = s.getOrElse("")
+    def phone(country: String = "US") = {
+      val parsed = phoneUtil.parse(v, country)
+      Option(phoneUtil.formatInOriginalFormat(parsed, country))
+    }
+
+    def phone: Option[String] = phone("US")
+
+    def postal_code(country: String) = {
+      Option(v)
+    }
+
+    def postal_code: Option[String] = postal_code("US")
+
+    def list(sep: String) = Option(v.split(sep).map(_.trim).mkString(","))
+
+    def list: Option[String] = list(",")
+    def asList(sep: String) = list.getOrElse(",").split(sep).toList
+
+    def asList: List[String] = asList(",")
+
+    def asJson = parse(v).getOrElse(Json.Null)
+  }
 
 }
